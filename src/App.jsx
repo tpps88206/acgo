0ba25887b61ddc821196b9962e07e31fdadc911a
Michael Hsu <cool96482@hotmail.com>
2023-09-25 18:12:16 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { NextUIProvider } from "@nextui-org/react";
+import './App.css'
+
+const App = () => {
+  return (
+    <NextUIProvider>
+      <h1>App</h1>
+    </NextUIProvider>	  
+  )
+}
+
+export default App
